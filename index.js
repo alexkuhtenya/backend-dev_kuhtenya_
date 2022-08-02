50bc23e0f171b4a12d17c694a3b737ceda351106
sasha <kuhtenyabiznes@gmail.com>
2022-08-02 17:02:16 +0300

    commit

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.get("/", (req, res) => {
+    return res.status(200).json({message: "привет"})
+})
 app.use(express.json())
 app.use("/api" , apiRouter)
 
