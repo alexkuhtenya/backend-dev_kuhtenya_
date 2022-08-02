350fe3ba7f06d5398918da9cb63e18898e4935a0
sasha <kuhtenyabiznes@gmail.com>
2022-08-02 16:47:02 +0300

    commit

--- models/Workers.js
+++ models/Workers.js
@@ -2,11 +2,6 @@ const {Schema, model} = require('mongoose')
 
 
 
-const Worker = new Schema({
-    fullName : {type: String , required : true},
-    workType : {type: String , required : true},
-    specification: {type: String , required : true},
-    IMG : {data : Buffer, contentType : String, required: false}
-})
+he
 
 module.exports = model('Workers' , Worker)
