bf28f47954f52f7fec483ea49a2716f6b02d4fbd
sasha <kuhtenyabiznes@gmail.com>
2022-08-11 15:59:47 +0300

    dev_uploads_etc

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -6,11 +6,11 @@ const path = require('path')
         class workerController{
             async addWorker(req, res) {
                 try {
-                    const {fullName, workType, bumpix ,description, inst } = req.body
+                   // const {fullName, workType, bumpix ,description, inst } = req.body
                     const {image} = req.files
                     let fileName = uuid.v4() + ".jpg"
            image.mv(path.resolve(__dirname,"..", "images", fileName))
-            const worker = new Worker ( {fullName, workType,description, bumpix , inst , image: fileName })
+            const worker = new Worker ( { image: fileName })
             await worker.save()
         console.log('работник добавлен успешно')
             return res.json({worker})
