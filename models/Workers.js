51bf481f1eb4bdd78a65fa722faf25986a3e3518
sasha <kuhtenyabiznes@gmail.com>
2022-08-11 16:23:32 +0300

    dev_uploads_etc

--- models/Workers.js
+++ models/Workers.js
@@ -3,11 +3,11 @@ const {Schema, model} = require('mongoose')
 
 
 const Worker = new Schema({
-    // fullName : {type: String , required : true},
-    // workType : {type: String , required : true},
-    // bumpix :{type: String},
-    // inst: {type: String},
-    // description :{type: String , required: true},
+    fullName : {type: String , required : true},
+    workType : {type: String , required : true},
+    bumpix :{type: String},
+    inst: {type: String},
+    description :{type: String , required: true},
     image : {type: String, required : true}
 })
 
