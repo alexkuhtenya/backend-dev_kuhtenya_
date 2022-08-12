ecf748a28ada3e49ed2238b36575afa7211eed00
sasha <kuhtenyabiznes@gmail.com>
2022-08-12 18:21:36 +0300

    dev_uploads_etc

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -10,7 +10,7 @@ const fs = require('fs')
                 try {
                     const imagee = {
                         img: {
-                            data : fs.readFileSync(path.join(__dirname +'../images' + req.body.filename) ),
+                            data : fs.readFileSync(path.join(__dirname +'../images/' + req.file.filename) ),
                             contentType: 'image/jpg'
                         }
                     }
