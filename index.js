6c49b4636dfbabe598109a6b8aa477e6964dd16c
sasha <kuhtenyabiznes@gmail.com>
2022-08-17 18:00:05 +0300

    `dev_uploads_etc`

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const {connect: connect1, mongo} = require("mongoose");
 const apiRouter = require('./routers/apiRouter')
 const path = require('path')
-const imgModel = require('./models/img');
 const bodyParser = require("body-parser");
 const fs = require('fs')
 require('dotenv').config()
@@ -13,19 +12,6 @@ const mongoURL = 'mongodb+srv://kuhtenya:123321qweH@cluster0.gc6qdci.mongodb.net
 const PORT = process.env.PORT || 5000
 const app = express()
 
-const multer = require('multer')
-const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
-            cb ( null , 'images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
-
-const upload = multer({storage : storage})
-
-
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(bodyParser.json())
@@ -33,33 +19,9 @@ app.use("/api" , apiRouter)
 
 
 
-app.post('/pp', upload.single('image'), (req, res, next) => {
-
-    const obj = {
-        name: req.body.name,
-        desc: req.body.desc,
-        img: {
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-            contentType: 'image/png'
-        }
-    }
-    imgModel.create(obj, (err, item) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // item.save();
-            res.redirect('/');
-        }
-    });
-});
 
 
 
-
-app.post('/upload', upload.single('image') , (req,res)=>{
-    res.json(req.file)
-} )
 
 app.get("/", (req, res) => {
     return res.status(200).json({message: "привет"})
