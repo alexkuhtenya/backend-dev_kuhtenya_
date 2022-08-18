b72446ff61f21ed89151564004578dc429d8a8b7
sasha <kuhtenyabiznes@gmail.com>
2022-08-18 17:42:21 +0300

    `dev_uploads_etc`

--- routers/workersRouter.js
+++ routers/workersRouter.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/workerController')
 const uploadMiddleware= require('../middleware/uploadMiddleware')
 
 router.post('/addWorker' ,uploadMiddleware.single('image'),controller.addWorker)
-router.post('/addImage', controller.imageForWorker)
+router.get('/addImage', controller.imageForWorker)
 router.get('/getWorker', controller.getWorkers)
 router.get('/Worker', controller.Worker)
 
