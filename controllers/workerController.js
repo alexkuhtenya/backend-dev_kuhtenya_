bb8e47dab0be1b041d03d3ce38831a8144140ebe
sasha <kuhtenyabiznes@gmail.com>
2022-08-21 18:17:07 +0300

    `dev_uploads_etc`

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -16,10 +16,6 @@ const fs = require('fs')
                         inst: req.body.inst ,
                         image : req.body.image
                     }
-
-
-
-
             const worker = new Worker (obj)
           await worker.save()
         console.log('работник добавлен успешно')
@@ -39,6 +35,45 @@ const fs = require('fs')
                     console.log(e)
                 }
     }
+
+    async editWorkers(req, res) {
+                try{
+                    const update = {
+                        fullName : req.body.fullName,
+                        workType: req.body.workType,
+                        bumpix: req.body.bumpix,
+                        inst: req.body.inst,
+                        description: req.body.description,
+                        image: req.body.image
+                    }
+                   const id = req.body._id
+                    Worker.findByIdAndUpdate(id, update , (err) =>  {
+                        if(!err){
+                            return res.status(200).json({message:"success"})
+                        } else{
+                            res.status(500).json(err.message)
+                        }
+                    })
+                } catch(e){
+                    res.status(500).json(e.message)
+                }
+            }
+
+            async delWorker(req, res){
+                try{
+                    const id = req.body._id
+                    Worker.findByIdAndRemove(id  , err => {
+                        if (!err) {
+                            res.status(200).json({message:"Работник успешно удален"})
+                        } else {
+                            res.status(500).json(err.message)
+                        }
+                        }
+                    )
+                } catch(e) {
+                    res.status(500).json(e.message)
+                }
+            }
 }
 
 
--- routers/workersRouter.js
+++ routers/workersRouter.js
@@ -4,6 +4,8 @@ const controller = require('../controllers/workerController')
 
 
 router.post('/addWorker' ,controller.addWorker)
+router.patch('/editWorker', controller.editWorkers)
+router.delete('/delWorker', controller.delWorker)
 router.get('/getWorker', controller.getWorkers)
 
 
